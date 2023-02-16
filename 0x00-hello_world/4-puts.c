6711991081fea406e61ffe7a53b6ecfc04c735de
Adhysamamaria <mariatamara1978@gmail.com>
2023-02-16 09:08:38 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - main block
- * return: 0
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
-{ 
-	puts("\"Programming is like develop a multingual puzzle");
+{
+	puts("\"programing is like building a multilingual puzzle");
 	return (0);
 }
