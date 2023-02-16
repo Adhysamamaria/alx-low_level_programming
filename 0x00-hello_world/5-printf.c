0f7959f4787176cb4e1d6b30f95e0898ae8c32fc
Adhysamamaria <mariatamara1978@gmail.com>
2023-02-16 08:31:19 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ 
+	printf("with proper grammr, but the outcome is a picee of art,\n");
+	return (0);
+}
